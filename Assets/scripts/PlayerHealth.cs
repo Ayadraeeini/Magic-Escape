f8e7c5eacbcd082e762956c967925608149c61a0
Ayadraeeini <aalraeeini@csuchico.edu>
2026-04-11 02:26:57 -0700

    fix main

--- Assets/scripts/PlayerHealth.cs
+++ Assets/scripts/PlayerHealth.cs
@@ -1,48 +1,27 @@
 //Eyad Al Raeeini - 02/17/2026
 //player health system
 using UnityEngine;
-<<<<<<< HEAD
-using System;
-
 public class PlayerHealth : MonoBehaviour
-=======
-public class PlayerHealth: MonoBehaviour
->>>>>>> 17fe806eb2a0836d47e5fcd17be94c9e70a3698b
 {
     public int maxHealth = 100;
     public int currentHealth;
 
-    public event Action<int, int> OnHealthChanged;
-
     void Awake()
     {
         currentHealth = maxHealth;
-        NotifyHealthChanged();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-
         if (currentHealth < 0)
             currentHealth = 0;
-
-        NotifyHealthChanged();
     }
 
     public void Heal(int amount)
     {
         currentHealth += amount;
-
         if (currentHealth > maxHealth)
             currentHealth = maxHealth;
-
-        NotifyHealthChanged();
-    }
-
-    void NotifyHealthChanged()
-    {
-        if (OnHealthChanged != null)
-            OnHealthChanged(currentHealth, maxHealth);
     }
 }
